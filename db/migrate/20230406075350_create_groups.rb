c2f9e20f56351fcaf1e816b3b891c12fc0b5b781
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-04-06 11:37:14 +0300

    Add models, migrations and validations

---
+++ db/migrate/20230406075350_create_groups.rb
@@ -0,0 +1,10 @@
+class CreateGroups < ActiveRecord::Migration[7.0]
+  def change
+    create_table :groups do |t|
+      t.string :name
+      t.string :icon
+      t.references :user, null: false, foriegn_key: { to_table: :users }
+      t.timestamps
+    end
+  end
+end
